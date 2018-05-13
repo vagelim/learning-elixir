d7a932028c2246853170a47d17c318c2c6b74ba3
vagelim <vageli.code@gmail.com>
2018-05-13 14:11:41 -0400

    cards project

---
+++ elixir-class/cards/lib/cards.ex
@@ -0,0 +1,95 @@
+defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+  Create a deck of cards
+
+  ## Examples
+
+      iex> Cards.create_deck
+      ["Ace of Spades", "Two of Spades", "Three of Spades", "Four of Spades",
+       "Five of Spades", "Ace of Clubs", "Two of Clubs", "Three of Clubs",
+       "Four of Clubs", "Five of Clubs", "Ace of Hearts", "Two of Hearts",
+       "Three of Hearts", "Four of Hearts", "Five of Hearts", "Ace of Diamonds",
+       "Two of Diamonds", "Three of Diamonds", "Four of Diamonds", "Five of Diamonds"]
+
+  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+       "#{value} of #{suit}"
+      end
+
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  @doc """
+  Checks a deck for a specific card.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck()
+      iex> Cards.contains?(deck, "Ace of Spades")
+      true
+  """
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  @doc """
+  Divides a deck into a hand and the remainder of the deck.
+  `hand_size` indicates how many cards you want in the hand.
+
+  ## Examples
+
+      iex> deck  = Cards.create_deck
+      iex> {hand,deck}  = Cards.deal(deck,1)
+      iex> hand
+      ["Ace of Spades"]
+  """
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _ }-> IO.puts("ERROR: #{filename} does not exist")
+    end 
+  end
+
+  @doc """
+  Creates a deck, shuffles it, and returns a tuple consisting of:
+    - a subset of the deck with a count equal to `hand_size`
+    - the deck minus the above hand
+  
+  ## Examples
+  
+        iex> Cards.create_hand(5)
+        {["Four of Clubs", "Two of Clubs", "Three of Spades", "Ace of Hearts",
+        "Three of Hearts"],
+       ["Five of Hearts", "Five of Diamonds", "Ace of Diamonds", "Two of Hearts",
+        "Four of Diamonds", "Three of Diamonds", "Four of Hearts", "Three of Clubs",
+        "Ace of Clubs", "Four of Spades", "Five of Clubs", "Ace of Spades",
+        "Two of Diamonds", "Two of Spades", "Five of Spades"]}
+  """
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
+
+end
